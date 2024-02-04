868ed16f365e7b09c64e56791d00731a3b2ff279
Franciscof11 <franciscogabrielf11@gmail.com>
2024-02-04 18:18:29 -0300

    refac project

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_animate/flutter_animate.dart';
-import 'package:responsive_builder/responsive_builder.dart';
-import 'package:upon_website/app/app.bottomsheets.dart';
-import 'package:upon_website/app/app.dialogs.dart';
-import 'package:upon_website/app/app.locator.dart';
-import 'package:upon_website/app/app.router.dart';
-import 'package:url_strategy/url_strategy.dart';
+import 'package:upon_site/pages/home_page.dart';
 
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  setPathUrlStrategy();
-  await setupLocator(stackedRouter: stackedRouter);
-  setupDialogUi();
-  setupBottomSheetUi();
-  runApp(const MainApp());
+void main() {
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ResponsiveApp(
-      builder: (_) => MaterialApp.router(
-        debugShowCheckedModeBanner: false,
-        routerDelegate: stackedRouter.delegate(),
-        routeInformationParser: stackedRouter.defaultRouteParser(),
-      ),
-    ).animate().fadeIn(
-          delay: const Duration(milliseconds: 50),
-          duration: const Duration(milliseconds: 400),
-        );
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Upon Systems',
+      theme: ThemeData(),
+      home: const HomePage(),
+    );
   }
 }
