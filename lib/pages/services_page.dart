40fcf84c6c8b01e845badc40a9b5ee1685b90170
Francisco Gabriel <franciscogabrielf11@gmail.com>
2024-06-28 20:33:13 -0300

    feat

--- lib/pages/services_page.dart
+++ lib/pages/services_page.dart
@@ -11,7 +11,7 @@ class ServicesPage extends StatefulWidget {
   State<ServicesPage> createState() => _ServicesPageState();
 }
 
-class _ServicesPageState extends State<ServicesPage> {
+class _ServicesPageState extends State<ServicesPage> {]
   @override
   Widget build(BuildContext context) {
     return responsiveChecker(
